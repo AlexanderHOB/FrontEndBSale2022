dda1cabc73f7fe6c6b18dfaeedcdeafd37a78351
AlexanderHOB <obispoa1@gmail.com>
2022-01-09 11:04:58 -0500

    Product Class, Static Methods and  Show DOM All Products Terminated

--- js/App.js
+++ js/App.js
@@ -1,4 +1,9 @@
+//URL API
 const URL = 'https://still-temple-32749.herokuapp.com/api/products';
 
 
-Product.getAllProducts(URL);
+//lOAD PAGE
+
+window.addEventListener("load", ()=>{
+    Product.getAllProducts(URL); // GET ALL PRODUCTS FROM API
+});
--- js/classes/Product.js
+++ js/classes/Product.js
@@ -1,16 +1,42 @@
 class Product{
-    
-    
+
+    // STATIC FUNCTION TO RENDER DOM
+    static showDOM(name,price,imageUrl){
+        const cards = document.querySelector('#cards'); // get card's container
+        cards.innerHTML += `
+            <div class="card" id="card">
+                <div class="card-img">
+                    <img src="${imageUrl}" alt="${name}">
+                </div>
+                <div class="card-title">
+                    <h3>${name}</h3>
+                </div>
+                <div class="card-actions">
+                    <div class="card-actions__price">
+                        <p>S./ ${price}</p>
+                    </div>
+                    <button class="card-actions__cart">
+                        <i class='bx bxs-cart-add' style='color:#000' ></i>
+                    </button>
+                </div>
+            </div>
+        `;
+    }
     static getAllProducts(url){
         fetch(url)
         .then(products => products.json())
-        .then(products => console.log(products))
+        .then(products => {
+            const data = products.data;
+            return data.map(({name,price,url_image}) =>{
+                this.showDOM(name,price,url_image);
+            });
+        })
         .catch(err => console.error(err));
     }
     static getProductById(url,id){
         fetch(`${url}/${id}`)
         .then(product => product.json())
-        .then(product => console.log(product))
+        .then(product => {product})
         .catch(err => console.error(err));
     }
 
